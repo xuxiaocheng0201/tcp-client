8bbd8858f791526c303e6e0ae864d8089506517e
xuxiaocheng <2762267080@qq.com>
2024-01-01 21:56:38 +0800

    feat(Client): 支持默认version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+* 支持默认version。
+
 ## [0.0.1] - 2024-1-1
 
 ### Added
--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,17 @@ pub extern crate serde;
 use async_trait::async_trait;
 use tcp_handler::common::{AesCipher, StarterError};
 use tokio::net::{TcpStream, ToSocketAddrs};
+use crate::network::start_client;
 
 pub use network::{send, recv};
-use crate::network::start_client;
 
 #[async_trait]
 pub trait Client {
     fn get_identifier(&self) -> &'static str;
 
-    fn get_version(&self) -> &'static str;
+    fn get_version(&self) -> &'static str {
+        env!("CARGO_PKG_VERSION", "You should define the version in the manifest or override this method.")
+    }
 
     async fn connect<A: ToSocketAddrs + Send>(&self, addr: A) -> Result<(TcpStream, AesCipher), StarterError> {
         start_client(self, addr).await
