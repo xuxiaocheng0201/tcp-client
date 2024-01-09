ed500d476e3453d365f42d0bc64f011fb1af954b
xuxiaocheng <2762267080@qq.com>
2024-01-09 13:00:00 +0800

    fix(Client): 扩展ClientBase的RW类型

--- src/client_base.rs
+++ src/client_base.rs
@@ -5,16 +5,16 @@ use tcp_handler::bytes::{Buf, BufMut, BytesMut};
 use tcp_handler::common::PacketError;
 use tcp_handler::flate2::Compression;
 use tcp_handler::variable_len_reader::{VariableReadable, VariableWritable};
-use tokio::net::tcp::{ReadHalf, WriteHalf};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use crate::mutable_cipher::MutableCipher;
 use crate::network::{recv, send};
 use crate::configuration::get_idle_sec;
 
 #[async_trait]
-pub trait ClientBase {
-    /*const*/ fn get_receiver<'a>(&'a mut self) -> (&'a mut ReadHalf, &MutableCipher);
+pub trait ClientBase<R, W> where R: AsyncReadExt + Unpin + Send, W: AsyncWriteExt + Unpin + Send {
+    /*const*/ fn get_receiver<'a>(&'a mut self) -> (&'a mut R, &MutableCipher);
 
-    /*const*/ fn get_sender<'a>(&'a mut self) -> (&'a mut WriteHalf, &MutableCipher);
+    /*const*/ fn get_sender<'a>(&'a mut self) -> (&'a mut W, &MutableCipher);
 
     async fn send<B: Buf + Send>(&mut self, message: &mut B) -> Result<(), PacketError> {
         let (sender, mutable_cipher) = self.get_sender();
